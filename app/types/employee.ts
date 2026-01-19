29af67c0c4efc1771fc447437eebce8851c6cb4e
shrudh14007 <shrudhir007@gmail.com>
2026-01-19 08:37:11 +0000

    stored all variables in separate file

---
+++ app/types/employee.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+  name: string
+  dateofbirth: string
+  gender: string
+  email: string
+  identification_number: string
+  Number: string
+  address: string
+  role: string
+  contract_type: string
+  document_required: string
+}
